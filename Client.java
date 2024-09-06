6fef2cb30d06889de44c6cf9d3f5d5c651a53c25
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-09-06 22:49:31 +0530

    Added reading and writing functions for client side

--- Client.java
+++ Client.java
@@ -1,6 +1,79 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
-    
-    public static void main(String[] args){
-        System.out.println("This is client");
+
+    Socket socket;
+    BufferedReader br;
+    PrintWriter out;
+
+    public Client() {
+        try {
+            System.out.println("Sending request to server");
+            socket = new Socket("127.0.0.1", 7777);
+            System.out.println("Connection done");
+
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            out = new PrintWriter(socket.getOutputStream());
+
+            StartReading();
+            StartWriting();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void StartReading() {
+        // thread - this will read data
+
+        Runnable r1 = () -> {
+            System.out.println("Reader started...");
+
+            while (true) {
+                try {
+                    String msg = br.readLine();
+                    if (msg.equals("exit")) {
+                        System.out.println("Server terminated the chat");
+                        break;
+                    }
+
+                    System.out.println("Server: " + msg);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        new Thread(r1).start();
+    }
+
+    public void StartWriting() {
+        // thread - this will send data to client
+        Runnable r1 = () -> {
+            System.out.println("Writer started...");
+
+            while (true) {
+                try {
+
+                    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                    String content = br1.readLine();
+                    out.println(content);
+                    out.flush();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(r1).start();
+    }
+
+    public static void main(String[] args) {
+        new Client();
     }
 }
